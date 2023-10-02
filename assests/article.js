621446d12294a23ef6dd9b00b6cee7455cb83233
Mohamad Alkahil <kahil408@gmail.com>
2023-10-02 17:09:48 -0400

    first commit

---
+++ assests/article.js
@@ -0,0 +1,53 @@
+
+function aboutDisplay() {
+     document.getElementById("about").style.display = "flex"; 
+}
+ 
+function aboutNone() {
+    document.getElementById("about").style.display = "none";
+}
+
+function projectsDisplay() {
+     document.getElementById("projects").style.display = "flex";
+}
+ 
+function projectsNone() {
+    document.getElementById("projects").style.display = "none";
+}
+
+function resumeDisplay() {
+    document.getElementById("resume").style.display = "flex";
+}
+
+function resumeNone() {
+    document.getElementById("resume").style.display = "none";
+}
+
+function contactDisplay() {
+    document.getElementById("contact").style.display = "flex"; 
+ }
+ 
+ function contactNone() {
+    document.getElementById("contact").style.display = "none";
+ }
+
+function FastAPI() {
+    document.getElementById("FastAPI").style.display = "flex";
+    ocument.getElementById("FastAPI").style.visibility = "visibile"
+}
+ 
+function BankingApplication() {
+    document.getElementById("BankingApplication").style.display = "flex";
+}
+
+function BookManagementSystem() {
+    document.getElementById("BookManagementSystem").style.display = "flex";
+}
+
+function DatingApplicationManagementSystem() {
+    document.getElementById("DatingApplicationManagementSystem").style.display = "flex";
+}
+
+
+
+
